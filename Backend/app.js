563758d74fca36fb5feee64e349e0c15a7f6529b
Krish Patel <105834521+Krishpatelx@users.noreply.github.com>
2024-01-12 12:55:23 -0500

    Added .env connections.

--- Backend/app.js
+++ Backend/app.js
@@ -9,14 +9,14 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cors())
-app.get('/',(request,res)=>{
+app.get('/',(req,res)=>{
     res.send('Hello World')
 
 })
 const server = () => {
 app.listen(PORT, ()=> {
-    console.log('listenig to port:',PORT);
-})
+    console.log('listenig to port:', PORT);
+  })
 }
 
 server ()
